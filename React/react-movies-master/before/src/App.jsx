c9412711fde5e9938a40fd054ad52443356dc9dd
MurshidMac <mh.murshidco.uk@outlook.com>
2017-04-13 21:55:29 +0530

    99xt-Sessions

---
+++ React/react-movies-master/before/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        <p>Hello World</p>
+        <img alt="placeholder" src={`statics/img/placeholder.png`} />
+      </div>
+    );
+  }
+}
+
+export default App
